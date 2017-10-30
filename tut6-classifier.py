ed428e0c26f0f2ecf96ef733fc2c0f91c822f790
Xiaochuang Han <xc@gatech.edu>
2017-10-30 19:30:15 -0400

    maybe this would work

--- tut6-classifier.py
+++ tut6-classifier.py
@@ -17,15 +17,17 @@ def make_features(corpus):
 from preproc import preproc
 _tweet_list, _location_list = preproc()
 
-tweet_list = make_features(_tweet_list)
+our_text = ['Atlanta Atlanta Farms Georgia Tech']
+tweet_list = make_features(_tweet_list + our_text)
 location_list = _location_list
+our_text_feature = tweet_list[-1]
+tweet_list = tweet_list[:-1]
 
 tweet_training_set, tweet_test_set, location_training_set, location_test_set = train_test_split(tweet_list, location_list, test_size=0.2, random_state=999)
-lr_clf = LogisticRegression(solver='saga', max_iter=3000)
+lr_clf = LogisticRegression(solver='saga', max_iter=5000)
 lr_clf.fit(tweet_training_set, location_training_set)
 predicted_location_test_set = lr_clf.predict(tweet_test_set)
 
 print(accuracy_score(location_test_set, predicted_location_test_set))
 
-text = ['Atlanta Atlanta Farms Georgia Tech']
-print(lr_clf.predict(make_features(text))) # THIS IS NOT WORKING
+print(lr_clf.predict(our_text_feature))
