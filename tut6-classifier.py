44a0c2c6342bb86c5aaad293b052b4cd928e2be6
Xiaochuang Han <xc@gatech.edu>
2017-10-30 19:13:46 -0400

    haha

--- tut6-classifier.py
+++ tut6-classifier.py
@@ -14,8 +14,11 @@ def make_features(corpus):
     vectorizer = CountVectorizer(tokenizer=Tweet_Tokenizer(), analyzer='word', min_df=0)
     return vectorizer.fit_transform(corpus)
 
-tweet_list = make_features(['A', 'B', 'C'])
-location_list = ['SE', 'NE', 'NE']
+from preproc import preproc
+_tweet_list, _location_list = preproc()
+
+tweet_list = make_features(_tweet_list)
+location_list = _location_list
 
 tweet_training_set, tweet_test_set, location_training_set, location_test_set = train_test_split(tweet_list, location_list, test_size=0.2, random_state=999)
 lr_clf = LogisticRegression(solver='saga', max_iter=3000)
